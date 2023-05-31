c95e2326b8b21a415dd64556a6a29b60e5a4d72f
arthurboucard <arthur.boucard@hotmail.fr>
2023-05-31 16:00:58 +0200

    added gravity CO2 sensor

--- ESP32-Controller/ESP32-Controller.ino
+++ ESP32-Controller/ESP32-Controller.ino
@@ -6,6 +6,7 @@
 #include "Adafruit_BME680.h"
 #include "secrets.h"
 
+#define CONNECTION_TIMEOUT 10
 
 // BME680 pins
 #define BME_SCK 18
@@ -13,22 +14,57 @@
 #define BME_MOSI 23
 #define BME_CS 5
 
-#define CONNECTION_TIMEOUT 10
-
 Adafruit_BME680 bme(BME_CS, BME_MOSI, BME_MISO,  BME_SCK);
 
-// Server info
-const char* serverUrl = "http://192.168.1.104:3000/measurements/create";
+// Gravity Infrared CO2 Sensor pins
+#define SENSOR_DATA_PIN 13
+#define INTERRUPT_NUMBER digitalPinToInterrupt(SENSOR_DATA_PIN)
+
+// Used in interrupt, calculate pulse width variable
+volatile unsigned long pwm_high_start_ticks=0, pwm_high_end_ticks=0;
+volatile unsigned long pwm_high_val=0, pwm_low_val=0;
+// interrupt flag
+volatile uint8_t flag=0;
 
 // Weight attribution
 const float HumidityWeight = 0.4;
 const float VOCWeight = 0.3;
 const float TemperatureWeight = 0.2;
+const float CO2Weight = 0.2;
 const float PressureWeight = 0.1;
+
+// Server info
+const char* serverUrl = "http://192.168.1.104:3000/measurements/create";
+
+void interrupt_change()
+{
+  if (digitalRead(SENSOR_DATA_PIN)) {
+    pwm_high_start_ticks = micros();
+    if (2 == flag) {
+      flag = 4;
+      if(pwm_high_start_ticks > pwm_high_end_ticks) {
+        pwm_low_val = pwm_high_start_ticks - pwm_high_end_ticks;
+      }
+    } else {
+      flag = 1;
+    }
+  } else {
+    pwm_high_end_ticks = micros();
+    if (1 == flag) {
+      flag = 2;
+      if (pwm_high_end_ticks > pwm_high_start_ticks) {
+        pwm_high_val = pwm_high_end_ticks - pwm_high_start_ticks;
+      }
+    }
+  }
+}
 
 void setup() {
   Serial.begin(115200);
   while (!Serial);
+
+  pinMode(SENSOR_DATA_PIN, INPUT);
+  attachInterrupt(INTERRUPT_NUMBER, interrupt_change, CHANGE);
 
   // Connect to WiFi
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
@@ -62,68 +98,114 @@ void setup() {
 
 void loop() {
   if (!bme.performReading()) {
-    Serial.println("Failed to perform reading :(");
+    Serial.println("BME680 failed to perform reading :");
     return;
   }
 
   Serial.print("Temperature = ");
   Serial.print(bme.temperature);
   Serial.println(" *C");
+  float temperature = bme.temperature;
 
   Serial.print("Pressure = ");
   Serial.print(bme.pressure / 100.0);
   Serial.println(" hPa");
+  float pressure = bme.pressure / 100.0;
 
   Serial.print("Humidity = ");
   Serial.print(bme.humidity);
   Serial.println(" %");
+  float humidity = bme.humidity;
 
   Serial.print("Gas = ");
   Serial.print(bme.gas_resistance * 0.001);
   Serial.println(" KOhms");
+  float gas = bme.gas_resistance * 0.001;
 
-  // Calculate normalized values
-  float normalizedHumidity = bme.humidity / 100.0;
-  float normalizedVOC = (bme.gas_resistance * 0.001 - 0) / (500 - 0);
-  float normalizedTemperature = (bme.temperature - (-10)) / (40 - 0);
-  float normalizedPressure = (bme.pressure / 100.0 - 900) / (1100 - 900);
+  float co2 = -1;
+  float iaqScore = -1;
 
-  // Calculate IAQ score
-  float iaqScore = (HumidityWeight * normalizedHumidity) + (VOCWeight * normalizedVOC) +
-                   (TemperatureWeight * normalizedTemperature) + (PressureWeight * normalizedPressure);
+  // Gravity Infrared CO2 Sensor output
+  if (flag == 4) {
+    flag = 1;
+    float pwm_high_val_ms = (pwm_high_val * 1000.0) / (pwm_low_val + pwm_high_val);
 
-  Serial.print("IAQ Score = ");
-  Serial.println(iaqScore);
-
-  // Create the HTTP client object and set the target URL + content type
-  HTTPClient http;
-  http.begin(serverUrl);
-  http.addHeader("Content-Type", "application/json");
-
-  // Create the JSON payload
-  String payload = "{\"temperature\":" + String(bme.temperature) +
-                  ",\"pressure\":" + String(bme.pressure / 100.0) +
-                  ",\"humidity\":" + String(bme.humidity) +
-                  ",\"gas\":" + String(bme.gas_resistance * 0.001) +
-                  ",\"co2\": 0" +
-                  ",\"iaq\":" + String(iaqScore) +
-                  "}";
-
-  // Send the POST request with the payload
-  int httpResponseCode = http.POST(payload);
-
-  // Check the response code
-  if (httpResponseCode == 201) {
-    String response = http.getString();
-    Serial.println("HTTP POST request successful. Response:");
-    Serial.println(response);
-  } else {
-    Serial.print("Error: ");
-    Serial.println(httpResponseCode);
+    if (pwm_high_val_ms < 0.01) {
+      Serial.println("Fault");
+    }
+    else if (pwm_high_val_ms < 80.00) {
+      Serial.println("preheating");
+    }
+    else if (pwm_high_val_ms < 998.00) {
+      float concentration = (pwm_high_val_ms - 2) * 5;
+      // Print pwm_high_val_ms
+      Serial.print("pwm_high_val_ms = ");
+      Serial.print(pwm_high_val_ms);
+      Serial.println("ms");
+      //Print CO2 concentration
+      Serial.print("CO2 concentration = ");
+      Serial.print(concentration);
+      Serial.println("ppm");
+      co2 = concentration;
+    } else {
+      Serial.println("Beyond the maximum range : 398~4980ppm");
+    }
   }
+  
+  // Verify all values are collected
+  if (temperature != -1 || pressure != -1 || humidity != -1 || gas != -1 || co2 != -1) {
 
-  // Clean up
-  http.end();
+    // Calculate normalized values
+    float normalizedHumidity = humidity / 100.0;
+    float normalizedVOC = (gas * 0.001 - 0) / (500 - 0);
+    float normalizedTemperature = (temperature - (-10)) / (40 - 0);
+    float normalizedPressure = (pressure / 100.0 - 900) / (1100 - 900);
+    float normalizedCO2 = (co2 - 400) / (5000 - 400);
+
+    // Calculate IAQ score
+    iaqScore = (HumidityWeight * normalizedHumidity) + (VOCWeight * normalizedVOC) + (CO2Weight * normalizedCO2) +
+                    (TemperatureWeight * normalizedTemperature) + (PressureWeight * normalizedPressure);
+
+    Serial.print("IAQ Score = ");
+    Serial.println(iaqScore);
+
+    // Create the HTTP client object and set the target URL + content type
+    HTTPClient http;
+    http.begin(serverUrl);
+    http.addHeader("Content-Type", "application/json");
+
+    // Create the JSON payload
+    String payload = "{\"temperature\":" + String(temperature) +
+                    ",\"pressure\":" + String(pressure) +
+                    ",\"humidity\":" + String(humidity) +
+                    ",\"gas\":" + String(gas) +
+                    ",\"co2\": " + String(co2) +
+                    ",\"iaq\":" + String(iaqScore) +
+                    "}";
+
+    // Send the POST request with the payload
+    int httpResponseCode = http.POST(payload);
+
+    // Check the response code
+    if (httpResponseCode == 201) {
+      String response = http.getString();
+      Serial.println("HTTP POST request successful. Response:");
+      Serial.println(response);
+    } else {
+      Serial.print("HTTP error: ");
+      Serial.println(httpResponseCode);
+    }
+
+    // Clean up
+    http.end();
+
+    // Reset all values
+    temperature = -1;
+    pressure = -1;
+    humidity = -1;
+    gas = -1;
+    co2 = -1;
+  }
 
   Serial.println();
   delay(2000);
