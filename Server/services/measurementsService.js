750240a907b0c5f1cceb5cec563d5d3cc8de52a2
arthurboucard <arthur.boucard@hotmail.fr>
2023-06-17 11:41:36 +0200

    added all routes to get specific element

--- Server/services/measurementsService.js
+++ Server/services/measurementsService.js
@@ -29,6 +29,78 @@ class MeasurementsService {
             throw new Error('Failed to get measurements');
         }
     }
+
+    async getCo2Measurements() {
+        try {
+            const query = `SELECT co2 FROM measurements`;
+
+            const [measurements] = await dbConnection.query(query);
+
+            return measurements;
+        } catch (error) {
+            throw new Error('Failed to get measurements');
+        }
+    }
+
+    async getTemperatureMeasurements() {
+        try {
+            const query = `SELECT temperature FROM measurements`;
+
+            const [measurements] = await dbConnection.query(query);
+
+            return measurements;
+        } catch (error) {
+            throw new Error('Failed to get measurements');
+        }
+    }
+
+    async getHumidityMeasurements() {
+        try {
+            const query = `SELECT humidity FROM measurements`;
+
+            const [measurements] = await dbConnection.query(query);
+
+            return measurements;
+        } catch (error) {
+            throw new Error('Failed to get measurements');
+        }
+    }
+
+    async getPressureMeasurements() {
+        try {
+            const query = `SELECT pressure FROM measurements`;
+
+            const [measurements] = await dbConnection.query(query);
+
+            return measurements;
+        } catch (error) {
+            throw new Error('Failed to get measurements');
+        }
+    }
+
+    async getGasMeasurements() {
+        try {
+            const query = `SELECT gas FROM measurements`;
+
+            const [measurements] = await dbConnection.query(query);
+
+            return measurements;
+        } catch (error) {
+            throw new Error('Failed to get measurements');
+        }
+    }
+
+    async getIaqMeasurements() {
+        try {
+            const query = `SELECT iaq FROM measurements`;
+
+            const [measurements] = await dbConnection.query(query);
+
+            return measurements;
+        } catch (error) {
+            throw new Error('Failed to get measurements');
+        }
+    }
 }
 
 module.exports = new MeasurementsService();
